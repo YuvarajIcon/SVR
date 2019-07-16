4064caca44584f518db57d8329879700d4b1cd4f
YuvarajIcon <32995080+YuvarajIcon@users.noreply.github.com>
2019-07-16 14:11:56 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'SVR',         # How you named your package folder (MyLib)
   packages = ['SVR'],   # Chose the same as "name"
-  version = '0.2',      # Start with a small number and increase it with every change you make
+  version = '0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'will retive latest version of softwatre',   # Give a short description about your library
   author = 'yuvaraj,naveen',                   # Type in your name
