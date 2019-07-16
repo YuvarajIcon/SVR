bc57957b16b16108c9094cd7f75ab0283680fa4f
YuvarajIcon <32995080+YuvarajIcon@users.noreply.github.com>
2019-07-16 14:01:45 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 setup(
-  name = 'Navic',         # How you named your package folder (MyLib)
-  packages = ['Navic'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  name = 'SVR',         # How you named your package folder (MyLib)
+  packages = ['SVR'],   # Chose the same as "name"
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'will retive latest version of softwatre',   # Give a short description about your library
   author = 'yuvaraj,naveen',                   # Type in your name
@@ -11,8 +11,7 @@ setup(
   download_url = 'https://github.com/YuvarajIcon/SVR.git',    # I explain this later on
   keywords = ['VERSION', 'RETRIVER', 'UPDATE','LATEST','NEW','SOFTWARE','WEBPAGE','WEBCRWALING'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'urllib',
-          're',
+          
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
